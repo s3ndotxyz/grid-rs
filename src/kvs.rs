e048458cd444b72e359c0fa920a94e1f3586e1de
hey-ewan <ewan@s3n.xyz>
2025-09-09 02:28:58 +0100

    chore: set up module functions

--- src/kvs.rs
+++ src/kvs.rs
@@ -1,2 +1,62 @@
-#[link(wasm_import_module = "host")]
-extern "C" {}
+use crate::region::Region;
+
+unsafe extern "C" {
+    fn kv_get(namespace_ptr: usize, key_ptr: usize) -> usize;
+    fn kv_set(namespace_ptr: usize, key_ptr: usize, value_ptr: usize);
+    fn kv_delete(namespace_ptr: usize, key_ptr: usize);
+    fn kv_delete_store(namespace_ptr: usize);
+    fn kv_commit(namespace_ptr: usize);
+}
+
+#[derive(Default)]
+pub struct Storage;
+
+impl Storage {
+    pub fn get(&self, namespace: &str, key: &str) -> Option<Vec<u8>> {
+        let namespace = Region::build(namespace.as_bytes());
+        let namespace_ptr = &*namespace as *const Region;
+        let key = Region::build(key.as_bytes());
+        let key_ptr = &*key as *const Region;
+
+        let value_ptr = unsafe { kv_get(namespace_ptr as usize, key_ptr as usize) };
+        if value_ptr == 0 {
+            return None;
+        }
+
+        unsafe { Some(Region::consume(value_ptr as *mut Region)) }
+    }
+
+    pub fn set(&self, namespace: &str, key: &str, value: &[u8]) {
+        let namespace = Region::build(namespace.as_bytes());
+        let namespace_ptr = &*namespace as *const Region;
+        let key = Region::build(key.as_bytes());
+        let key_ptr = &*key as *const Region;
+        let value = Region::build(value);
+        let value_ptr = &*value as *const Region;
+
+        unsafe { kv_set(namespace_ptr as usize, key_ptr as usize, value_ptr as usize) };
+    }
+
+    pub fn delete(&self, namespace: &str, key: &str) {
+        let namespace = Region::build(namespace.as_bytes());
+        let namespace_ptr = &*namespace as *const Region;
+        let key = Region::build(key.as_bytes());
+        let key_ptr = &*key as *const Region;
+
+        unsafe { kv_delete(namespace_ptr as usize, key_ptr as usize) };
+    }
+
+    pub fn delete_store(&self, namespace: &str) {
+        let namespace = Region::build(namespace.as_bytes());
+        let namespace_ptr = &*namespace as *const Region;
+
+        unsafe { kv_delete_store(namespace_ptr as usize) };
+    }
+
+    pub fn commit(&self, namespace: &str) {
+        let namespace = Region::build(namespace.as_bytes());
+        let namespace_ptr = &*namespace as *const Region;
+
+        unsafe { kv_commit(namespace_ptr as usize) };
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,60 @@
 //!
 //! ## Features
 //!
-//! * *kvs*. Provides access to the native in-runtime key-value store. You should be able tomanage
-//! keys, values, and stores from within your functions. *Note:* stores are currently only
-//! available, one to each function.
+//! * *kvs*. Provides access to the native in-runtime key-value store. You should be able tomanage keys, values, and stores from within your functions. *Note:* stores are currently only available, one to each function.
 //! * *clock*. We've enabled secure time for you to do things like event scheduling.
 //!
 //! We have plans to enable a few other things such as message queues and web-sockets in the near
 //! future. Stay tuned!
 //!
 
-pub mod clock;
+// pub mod clock;
 pub mod kvs;
+pub mod region;
+
+use region::Region;
+
+unsafe extern "C" {
+    fn input_ptr() -> usize;
+    fn set_output(ptr: usize);
+}
+
+pub struct Input(Vec<u8>);
+
+impl Input {
+    #[unsafe(no_mangle)]
+    pub extern "C" fn read_all() -> *mut Region {
+        let ptr = unsafe { input_ptr() };
+        if ptr == 0 {
+            return std::ptr::null_mut();
+        }
+        ptr as *mut Region
+    }
+}
+
+impl AsRef<[u8]> for Input {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+#[derive(Default)]
+pub struct Output(Vec<u8>);
+
+impl Output {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn write_all(&mut self, data: &[u8]) {
+        self.0.extend_from_slice(data);
+    }
+}
+
+impl Drop for Output {
+    fn drop(&mut self) {
+        let region_ptr = Region::release_buffer(std::mem::take(&mut self.0));
+        unsafe { set_output(region_ptr as usize) };
+    }
+}
+
