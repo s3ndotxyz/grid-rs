411232f2dfa5b081318957ae52e2c1f0891b57db
hey-ewan <ewan@s3n.xyz>
2025-09-12 22:23:40 +0100

    init examples

---
+++ examples/storage/src/main.rs
@@ -0,0 +1,28 @@
+use grid_rs::{kvs::Storage};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct MyInput {
+    username: String,
+    data: Vec<u8>,
+}
+
+#[derive(Serialize)]
+struct MyOutput {
+    status: String,
+    result: Vec<u8>,
+}
+
+#[grid_rs::main]
+fn main(input: &[u8]) -> Result<Vec<u8>, String> {
+    let input: MyInput = serde_json::from_slice(input).unwrap();
+    
+    Storage::set(&input.username, &input.data);
+    
+    let output = MyOutput {
+        status: "success".to_string(),
+        result: input.data,
+    };
+    
+    Ok(serde_json::to_vec(&output).unwrap())
+}
